47b394c7797ce47fc2a11004a0856a249bd3a945
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-07-14 13:14:48 +0900

    feat: passage 제 2정규화 작업 진행. description 테이블(Entity) 추가

---
+++ src/main/java/com/cj/genieq/passage/entity/DescriptionEntity.java
@@ -0,0 +1,42 @@
+package com.cj.genieq.passage.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+@DynamicInsert
+@Table(name = "description")
+public class DescriptionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ✅ MariaDB는 IDENTITY
+    @Column(name = "des_code") // ✅ 소문자
+    private Long desCode;
+
+    @Column(name = "pas_type")
+    private String pasType;
+
+    @Column(name = "pas_keyword")
+    private String keyword;
+
+    @Lob
+    @Column(name = "pas_gist", columnDefinition = "LONGTEXT")
+    private String gist;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pas_code") // ✅ FK도 소문자
+    private PassageEntity passage;
+
+}
